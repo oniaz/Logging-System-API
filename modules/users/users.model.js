1f7b632fd5ec3de8e16bc972d84be6fd1c6a9374
oniaz <32391049+oniaz@users.noreply.github.com>
2026-05-25 15:02:09 +0300

    chore: prevent model overwrite errors in dev with mongoose.models check

--- modules/users/users.model.js
+++ modules/users/users.model.js
@@ -30,4 +30,4 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export default mongoose.model("User", UserSchema);
+export default mongoose.models.User || mongoose.model("User", UserSchema);
