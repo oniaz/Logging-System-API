00bd3167a6bab86254256878c452a04f96b8979c
oniaz <32391049+oniaz@users.noreply.github.com>
2026-05-25 05:45:39 +0300

    implement database connection. add dotenv and mongoose

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 import express from "express";
 import morgan from "morgan";
+import dotenv from "dotenv";
 
 import usersRoutes from "./modules/users/users.route.js";
 import applicationsRoutes from "./modules/applications/applications.route.js";
 import logsRoutes from "./modules/logs/logs.route.js";
+import connectDB from "./config/db.js";
 
+
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 
 app.use(morgan("dev"));
 app.use(express.json());
-
-
-const PORT = 3000;
-
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("Logging System API");
