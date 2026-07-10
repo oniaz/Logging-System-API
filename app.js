a68930b04ef1e2f9f2ad2695e365dbbd2787be40
Omnia <32391049+oniaz@users.noreply.github.com>
2026-07-10 00:02:54 +0000

    Allow All Domains for Cors

--- app.js
+++ app.js
@@ -21,20 +21,22 @@ app.use(express.json());
 app.use(cookieParser());
 await connectDB();
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin) return callback(null, true);
 
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
+//       if (allowedOrigins.includes(origin)) {
+//         return callback(null, true);
+//       }
 
-      return callback(new Error("Not allowed by CORS"));
-    },
-    credentials: true,
-  })
-);
+//       return callback(new Error("Not allowed by CORS"));
+//     },
+//     credentials: true,
+//   })
+// );
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Logging System API");
